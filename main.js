e4c1de0472840bcf1d30ae6cf888c8c3ab30b768
d4rtzzz <98066938+d4rtzzzzzz@users.noreply.github.com>
2025-11-12 18:04:13 -0800

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ loader.load(
     document.getElementById("loading")?.remove();
     const model = gltf.scene;
     model.position.set(0, 0, 0);
-    model.scale.set(0.5, 0.5, 0.5);
+    model.scale.set(100, 100, 100);
 
     // Ajustar materiales para reflejar la luz correctamente
     model.traverse((child) => {
